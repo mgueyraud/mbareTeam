45df3106f66e596d60f2ce26b6fe74f43a493840
javs98 <javier_lopez-1998@hotmail.com>
2023-09-08 10:15:12 -0400

    creacion permisos

--- app/routes/create.role.tsx
+++ app/routes/create.role.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { type ActionArgs, json } from "@remix-run/node";
 import { Form, useLoaderData, useNavigation } from "@remix-run/react";
 import { authenticator } from "~/services/auth.server";
 import type { User } from "@prisma/client";
 import { prisma } from "~/utils/db.server";
+import { Checkbox } from "@/components/ui/checkbox"
 
 export const loader = async ({ request }: ActionArgs) => {
   // authenticator.isAuthenticated function returns the user object if found
@@ -37,7 +37,17 @@ export default function CreateRole() {
           <Input id="rolname" name="rolname" />
         </div>
         <div className="grid w-full max-w-sm items-center gap-1.5 mt-5">
-
+        {data.map((data) => (
+          <div key={data.id}>
+            <Checkbox id={data.id}/>
+            <label
+              htmlFor="terms2"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ml-3"
+            >
+              {data.name}
+            </label>
+          </div>  
+        ))}       
         </div>
         <Button
           className="mt-4"
