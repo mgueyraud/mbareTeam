910e813ea2d67400f3698d21223d4be84edcbadb
mgueyraud <mgueyraud.junior@gmail.com>
2023-11-03 12:25:09 -0300

    Deleted prisma migrations folder

--- prisma/migrations/20230821020608_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE `User` (
-    `name` VARCHAR(191) NOT NULL,
-    `username` VARCHAR(191) NOT NULL,
-
-    UNIQUE INDEX `User_username_key`(`username`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
