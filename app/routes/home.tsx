1110b47a2f6a353de039f4877a1567e14bc9b2f2
Exodiapy <ortiz.emiliano44@gmail.com>
2023-12-13 22:38:35 -0300

    comment button

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { redirect, type ActionArgs, json } from "@remix-run/node";
 import { Form, Link, useLoaderData } from "@remix-run/react";
-import { FilePlus2, MoveUpRight, Search, ThumbsDown, ThumbsUp } from "lucide-react";
+import { FilePlus2, MoveUpRight, Search, ThumbsDown, ThumbsUp, MessageCircle } from "lucide-react";
 import { prisma } from "~/utils/db.server";
 import {
   Card,
@@ -16,6 +16,8 @@ import DropdownMenu from "@/components/ui/dropdownmenu";
 import { ESTADO_PUBLICADO } from "~/utils/constants";
 import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
+import { authenticator } from "~/services/auth.server";
+import { User } from "@prisma/client";
 
 // file: app/routes/dashboard.js
 export const action = async({request, params}: ActionArgs) => {
@@ -57,6 +59,11 @@ export const loader = async ({ request }: ActionArgs) => {
   const url = new URL(request.url);
   const categoryId = url.searchParams.get('categoryId');
   const name = url.searchParams.get('name');
+  
+  const user = (await authenticator.isAuthenticated(request, {
+    failureRedirect: "/",
+  })) as User;
+
   const contents = await prisma.content.findMany({
     where: {
       AND: [
@@ -85,48 +92,64 @@ export const loader = async ({ request }: ActionArgs) => {
   const categorias = await prisma.category.findMany( );
   return {
     contents,
-    categorias
+    categorias,
+    user,
   };
 };
-const HomeCard = ({ content }: { content: any}) => (
-  <Card className="w-[350px]" key={content.id}>
-    <CardHeader className="justify-between flex-row">
-      <CardTitle>{content.title}</CardTitle>
-      <MoveUpRight width={20} height={20} />
-    </CardHeader>
-    <CardContent>
-      <div className="flex flex-row justify-between items-center">
-        <CardDescription>{content.description}</CardDescription>
-        <div>
-          <Form method="POST">
-            <Input type="hidden" name="contentId" value={content.id}/>
-            <Button size="sm" variant="outline" type="submit" name="likeCount" className="mr-1" value={content.likeCount + 1}><ThumbsUp /></Button>
-          </Form>
-          <Form method="POST">
-            <Input type="hidden" name="contentId" value={content.id}/>
-            <Button size="sm" variant="outline" type="submit" name="dislikeCount" value={content.dislikeCount + 1}><ThumbsDown /></Button>
-          </Form>
+const HomeCard = ({ content, user }: { content: any, user: any}) => {
+  
+  return (
+    <Card className="w-[350px]" key={content.id} >
+      <CardHeader className="justify-between flex-row">
+        <CardTitle>{content.title}</CardTitle>
+        <MoveUpRight width={20} height={20} />
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-row items-center">
+          <div className="w-3/4">
+            <CardDescription>{content.description}</CardDescription>
+          </div>
+          <div className="w-1/4">
+            <div className="mb-1">
+              <Badge variant="secondary" className="w-[4rem] flex flex-row items-center text-gray-400">
+                <span className="text-sm mr-2">
+                  {content.likeCount} 
+                </span>
+                <ThumbsUp size={20} />
+              </Badge>
+            </div>
+            <div>
+              <Badge variant="secondary" className="w-[4rem] flex flex-row items-center text-gray-400">
+                <span className="text-sm mr-2">
+                  {content.dislikeCount} 
+                </span>
+                <ThumbsDown size={20} />
+              </Badge>
+            </div>
+          </div>
         </div>
-      </div>
-    </CardContent>
-    <CardFooter>
-      <Badge variant="secondary" className="flex flex-row items-center text-gray-400">
-        <span className="text-sm mr-2">
-          {content.likeCount} 
-        </span>
-        <ThumbsUp size={20} />
-      </Badge>
-      <Badge variant="secondary" className="flex flex-row items-center text-gray-400">
-        <span className="text-sm mr-2">
-          {content.dislikeCount} 
-        </span>
-        <ThumbsDown size={20} />
-      </Badge>
-    </CardFooter>
-  </Card>
-);
+      </CardContent>
+      <CardFooter>
+        <Form method="POST">
+          <Input type="hidden" name="contentId" value={content.id}/>
+          <Button size="sm" variant="outline" type="submit" name="likeCount" className="mr-1" value={content.likeCount + 1}><ThumbsUp /></Button>
+        </Form>
+        <Form method="POST">
+          <Input type="hidden" name="contentId" value={content.id}/>
+          <Button size="sm" variant="outline" type="submit" name="dislikeCount" value={content.dislikeCount + 1}><ThumbsDown /></Button>
+        </Form>
+        <Button variant="outline" size="sm" className="ml-1" 
+          // onClick={openCommentModal}
+          disabled={user ? false : true}
+        >
+          <MessageCircle />
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
 const Home = () => {
-  const { contents , categorias } = useLoaderData<typeof loader>();
+  const { contents , categorias, user } = useLoaderData<typeof loader>();
   const categoria_select=function(option : ActionArgs){
     console.log("opcion: ", option);
   }
@@ -154,7 +177,7 @@ const Home = () => {
               <h2 className="font-bold">Contenido destacado</h2>
               <div className="mt-5 flex gap-8 flex-wrap">
                 {contenidoDestacado.map((content) => (
-                  <HomeCard content={content} key={content.id}/>
+                  <HomeCard content={content} user={user} key={content.id}/>
                 ))}
               </div>
             </div>
@@ -162,7 +185,7 @@ const Home = () => {
               <h2 className="font-bold">Contenido</h2>
               <div className="mt-5 flex gap-8 flex-wrap">
                 {contenidoRestante.map((content) => (
-                  <HomeCard content={content} key={content.id}/>
+                  <HomeCard content={content} user={user} key={content.id}/>
                 ))}
               </div>
             </div>
